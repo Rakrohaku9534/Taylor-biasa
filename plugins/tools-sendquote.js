1d86c7e33168d254c21edf11763748b87e093087
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-10-06 18:45:51 +0800

    Add files via upload

--- plugins/tools-sendquote.js
+++ plugins/tools-sendquote.js
@@ -1,13 +1,7 @@
-let handler = async (m, { conn, text, command, usedPrefix, isAdmin, isOwner }) => {
-    if (m.isGroup) {
-        if (!(isAdmin || isOwner)) {
-            dfail('admin', m, conn)
-            throw false
-        }
-    }
+let handler = async (m, { conn, text, command, usedPrefix }) => {
     if (!m.quoted) throw 'balas pesannya!'
     try {
-		var q = this.serializeM(await (this.serializeM(await m.getQuotedObj())).getQuotedObj())
+		let q = await conn.serializeM(await(await conn.serializeM(await m.getQuotedObj())).getQuotedObj())
 		if (!q) throw 'pesan yang anda reply tidak mengandung reply!'
 		await q.copyNForward(m.chat, true)
 	} catch (e) {
